43e65ebbf495c9082563e80569e08ed9cbf6cd63
anujott-codes <anujot.workss@gmail.com>
2026-02-23 00:53:58 +0530

    fix: mocking mlflow fix in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,34 +46,32 @@ class FakePredictionService:
 
 @pytest.fixture(autouse=True)
 def mock_mlflow():
+    from unittest.mock import patch, MagicMock
 
-    with patch("mlflow.pyfunc.load_model") as mock_load, \
-         patch("mlflow.tracking.MlflowClient") as mock_client_class:
+    with patch("src.api.services.model_service.mlflow.pyfunc.load_model") as mock_load, \
+         patch("src.api.services.model_service.MlflowClient") as mock_client_class:
 
-        # ---- Mock Model ----
+        # ---- Fake Model ----
         mock_model = MagicMock()
         mock_model.predict.return_value = [0.8]
 
-        # ---- Mock Signature ----
-        def fake_col(name):
-            class Col:
-                def __init__(self, name):
-                    self.name = name
-
-            return Col(name)
+        # ---- Fake Signature ----
+        class FakeCol:
+            def __init__(self, name):
+                self.name = name
 
         fake_signature = MagicMock()
         fake_signature.inputs = [
-            fake_col("age"),
-            fake_col("gender"),
-            fake_col("tenure"),
-            fake_col("usage_frequency"),
-            fake_col("support_calls"),
-            fake_col("payment_delay"),
-            fake_col("subscription_type"),
-            fake_col("contract_length"),
-            fake_col("total_spend"),
-            fake_col("last_interaction"),
+            FakeCol("age"),
+            FakeCol("gender"),
+            FakeCol("tenure"),
+            FakeCol("usage_frequency"),
+            FakeCol("support_calls"),
+            FakeCol("payment_delay"),
+            FakeCol("subscription_type"),
+            FakeCol("contract_length"),
+            FakeCol("total_spend"),
+            FakeCol("last_interaction"),
         ]
 
         fake_metadata = MagicMock()
@@ -82,11 +80,10 @@ def mock_mlflow():
         mock_model.metadata = fake_metadata
         mock_load.return_value = mock_model
 
-        # ---- Mock MlflowClient ----
+        # ---- Fake Registry Client ----
         mock_client = MagicMock()
         mock_version = MagicMock()
         mock_version.version = "5"
-
         mock_client.get_model_version_by_alias.return_value = mock_version
         mock_client_class.return_value = mock_client
 
