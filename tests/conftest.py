5804b4101e2e8f50a4450456b2e102df3142734f
anujott-codes <anujot.workss@gmail.com>
2026-02-23 00:44:08 +0530

    test isolation by mocking mlflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,24 @@
 import pytest
 from fastapi.testclient import TestClient
+from unittest.mock import patch, MagicMock
 
-from src.api.main import app
+from src.api.main import create_app
 from src.api.dependencies import get_prediction_service
 
 
-# Fake Service for testing api endpoints
+# Fake Services
 class FakeModelService:
     def __init__(self):
         self.model = True
 
     def predict(self, df):
-        # return probability list
         return [0.8 for _ in range(len(df))]
 
     def get_model_info(self):
         return {
             "model_name": "CustomerChurnModel",
             "version": "5",
-            "alias": "champion"
+            "alias": "champion",
         }
 
 
@@ -30,7 +30,7 @@ class FakePredictionService:
         return {
             "prediction": 1,
             "probability": 0.8,
-            "churn": True
+            "churn": True,
         }
 
     def predict_batch(self, df):
@@ -38,14 +38,65 @@ class FakePredictionService:
             {
                 "prediction": 1,
                 "probability": 0.8,
-                "churn": True
+                "churn": True,
             }
             for _ in range(len(df))
         ]
 
 
+@pytest.fixture(autouse=True)
+def mock_mlflow():
+
+    with patch("mlflow.pyfunc.load_model") as mock_load, \
+         patch("mlflow.tracking.MlflowClient") as mock_client_class:
+
+        # ---- Mock Model ----
+        mock_model = MagicMock()
+        mock_model.predict.return_value = [0.8]
+
+        # ---- Mock Signature ----
+        def fake_col(name):
+            class Col:
+                def __init__(self, name):
+                    self.name = name
+
+            return Col(name)
+
+        fake_signature = MagicMock()
+        fake_signature.inputs = [
+            fake_col("age"),
+            fake_col("gender"),
+            fake_col("tenure"),
+            fake_col("usage_frequency"),
+            fake_col("support_calls"),
+            fake_col("payment_delay"),
+            fake_col("subscription_type"),
+            fake_col("contract_length"),
+            fake_col("total_spend"),
+            fake_col("last_interaction"),
+        ]
+
+        fake_metadata = MagicMock()
+        fake_metadata.signature = fake_signature
+
+        mock_model.metadata = fake_metadata
+        mock_load.return_value = mock_model
+
+        # ---- Mock MlflowClient ----
+        mock_client = MagicMock()
+        mock_version = MagicMock()
+        mock_version.version = "5"
+
+        mock_client.get_model_version_by_alias.return_value = mock_version
+        mock_client_class.return_value = mock_client
+
+        yield
+
+
 @pytest.fixture
 def client():
+    app = create_app()
+
     def override_dependency():
         return FakePredictionService()
 
